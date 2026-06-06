c171039052e0f61cdf1097d014228dbdafaaec4c
AizahR <aizrizwan16@gamil.com>
2026-06-06 14:05:10 -0500

    imported data

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedropathing/ParkingAutoRoute.java
@@ -0,0 +1,26 @@
+ackage org.firstinspires.ftc.teamcode.opmode.auto;
+
+import android.provider.ContactsContract;
+
+import com.pedropathing.follower.Follower;
+import com.pedropathing.geometry.BezierCurve;
+import com.pedropathing.geometry.BezierLine;
+import com.pedropathing.geometry.Pose;
+import com.pedropathing.paths.PathChain;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.seattlesolvers.solverslib.command.CommandScheduler;
+import com.seattlesolvers.solverslib.command.InstantCommand;
+import com.seattlesolvers.solverslib.command.SequentialCommandGroup;
+import com.seattlesolvers.solverslib.command.WaitCommand;
+import com.seattlesolvers.solverslib.command.WaitUntilCommand;
+import com.seattlesolvers.solverslib.command.button.Trigger;
+import com.seattlesolvers.solverslib.pedroCommand.FollowPathCommand;
+
+import org.firstinspires.ftc.teamcode.base.DataStorage;
+import org.firstinspires.ftc.teamcode.base.RobotBase;
+
+@Autonomous(name = "Park In Blue Zone")
+public class BlueFarAuto extends OpMode {
+
+}
