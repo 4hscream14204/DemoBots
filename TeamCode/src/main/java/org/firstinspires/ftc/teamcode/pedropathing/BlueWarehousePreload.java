306817f1a15e80f4019a386f4ead5af7fe530041
AizahR <aizrizwan16@gamil.com>
2026-06-11 13:56:08 -0500

    worked on autoRoutes

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedropathing/BlueWarehousePreload.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedropathing/BlueWarehousePreload.java
@@ -1,10 +1,7 @@
 package org.firstinspires.ftc.teamcode.pedropathing;
 
 import static org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.hardwareMap;
-import static org.firstinspires.ftc.teamcode.subsystems.Arm.ArmPosition.HIGH;
-
-import android.content.Context;
-import android.content.Intent;
+import static org.firstinspires.ftc.teamcode.subsystems.Arm.ArmPosition.HOME;
 
 import com.pedropathing.follower.Follower;
 import com.pedropathing.geometry.BezierLine;
@@ -14,67 +11,62 @@ import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.seattlesolvers.solverslib.command.CommandScheduler;
 import com.seattlesolvers.solverslib.command.InstantCommand;
 import com.seattlesolvers.solverslib.command.SequentialCommandGroup;
-import com.seattlesolvers.solverslib.command.Subsystem;
-import com.seattlesolvers.solverslib.command.SubsystemBase;
 import com.seattlesolvers.solverslib.command.WaitCommand;
 import com.seattlesolvers.solverslib.pedroCommand.FollowPathCommand;
 
 import org.firstinspires.ftc.teamcode.subsystems.Arm;
 import org.firstinspires.ftc.teamcode.subsystems.Intake;
-import org.firstinspires.ftc.teamcode.teleop.SimonTeleOp;
 
-@Autonomous(name = "Blue Preload and Warehouse Park ")
+@Autonomous (name ="Blue Preload and Warehouse Park")
 public class BlueWarehousePreload {
-    Follower follower;
-    SequentialCommandGroup path;
 
+Follower follower;
+SequentialCommandGroup path;
 
-    Pose startPose = new Pose(5, 35, Math.toRadians(0));
-    Pose goesToHub = new Pose(47,33,Math.toRadians(90));
-    Pose endPose = new Pose(35, 12, Math.toRadians(270));
+Pose startPose = new Pose(5,89, Math.toRadians(90));
+Pose linesUpToHub = new Pose(48,7,Math.toRadians(270));
+Pose hubToWall = new Pose(5,86,Math.toRadians(90));
+Pose endPose = new Pose(5,134,Math.toRadians(90));
 
-    BezierLine startsAndGoingToLineUp = new BezierLine(startPose, goesToHub);
-    BezierLine GoesToParkInZone = new BezierLine(goesToHub, endPose);
+BezierLine startToHub = new BezierLine(startPose,linesUpToHub);
+BezierLine goesToParkInZone = new BezierLine(hubToWall,endPose);
 
+PathChain StartToHub;
+PathChain HubToPark
 
-    PathChain startToHub;
-    PathChain parksInZone;
+@Override
+public void init() {
+    CommandScheduler.getInstance().reset();
+    follower = Constants.createFollower(hardwareMap);
 
-    @Override
-    public void init() {
-        CommandScheduler.getInstance().reset();
-        follower = Constants.createFollower(hardwareMap);
+    StartToHub = follower.pathBuilder()
+            .addPath(startToHub)
+            .setLinearHeadingInterpolation(Math.toRadians(90),Math.toRadians(270))
+            .build();
+    HubToPark = follower.pathBuilder()
+            .addPath(goesToParkInZone)
+            .setLinearHeadingInterpolation(Math.toRadians(270),Math.toRadians(90))
+            .build();
 
-
-        startToHub = follower.pathBuilder()
-                .addPath(startsAndGoingToLineUp)
-                .setLinearHeadingInterpolation(Math.toRadians(0), Math.toRadians(90))
-                .build();
-        parksInZone = follower.pathBuilder()
-                .addPath(GoesToParkInZone)
-                .setLinearHeadingInterpolation(Math.toRadians(90), Math.toRadians(270))
-                .build();
-
-        path = new SequentialCommandGroup(
-                new InstantCommand(()->Intake),
-                new FollowPathCommand(follower, startToHub, true, 1),
-                new InstantCommand(()->Arm.ArmPosition.HIGH),
-                new InstantCommand(()->Intake),
-                new WaitCommand(500),
-                new InstantCommand(()->Arm.ArmPosition.HOME),
-                new FollowPathCommand(follower, parksInZone, true, 1)
-        );
-    }
-    @Override
-    public void start(){
-        follower.setStartingPose(startPose);
-        path.schedule();
-    }
-
-    @Override
-    public void loop() {
-        CommandScheduler.getInstance().run();
-        follower.update();
-    }
+    path = new SequentialCommandGroup(
+            new InstantCommand(()-> hardwareMap.get(Intake.class,Intake,1)),
+            new FollowPathCommand(follower, StartToHub,true,1),
+            new InstantCommand(()-> hardwareMap.get(Intake.class,Intake,1)),
+            new WaitCommand(500),
+            new InstantCommand(()-> hardwareMap.get(Arm.class, Arm.ArmPosition,HOME)),
+            new FollowPathCommand(follower,HubToPark,true,1)
+    );
 }
 
+@Override
+public void start() {
+    follower.setStartingPose(startPose);
+    path.schedule();
+}
+
+@Override
+public void loop() {
+    CommandScheduler.getInstance().run();
+    follower.update();
+}
+}
