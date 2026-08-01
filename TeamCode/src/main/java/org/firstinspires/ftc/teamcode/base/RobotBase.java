a34aee6bfdf4b17a0d1f42c701abf39ec87a6fe1
Liams-The-Best-At-Redstone <liamverheecke@4Hscream.org>
2026-07-31 19:56:49 -0500

    Built Chassis

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/base/RobotBase.java
@@ -0,0 +1,18 @@
+package org.firstinspires.ftc.teamcode.base;
+
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+import org.firstinspires.ftc.teamcode.subsystems.Chassis;
+
+public class RobotBase {
+    public Chassis chassisSubsystem;
+
+    public RobotBase(HardwareMap hwMap) {
+        chassisSubsystem = new Chassis(
+                hwMap.dcMotor.get("frontLeftMotor"),
+                hwMap.dcMotor.get("frontRightMotor"),
+                hwMap.dcMotor.get("backLeftMotor"),
+                hwMap.dcMotor.get("backRightMotor"));
+
+    }
+}
