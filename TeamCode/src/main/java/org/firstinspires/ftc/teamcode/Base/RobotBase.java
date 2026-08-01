59f8e754de3f8010f5b660a992fa05f74d37bd60
lauren <LaurenLehane@4hscream.org>
2026-07-31 19:56:06 -0500

    Programming Workshop

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Base/RobotBase.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Base/RobotBase.java
@@ -1,15 +1,19 @@
 package org.firstinspires.ftc.teamcode.Base;
 
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+import Subsystem.Chassis;
+
 public class RobotBase {
-    public Chassis chasssisSubsystem;
-    public RobotBase(Hardwaremap hwMap){
-        chasssisSubsystem = new Chassis(
+    public Chassis chassisSubsystem;
+    public RobotBase(HardwareMap hwMap){
+        chassisSubsystem = new Chassis(
                 hwMap.dcMotor.get("frontLeftMotor"),
-                hwMap.dcMotor.get("frontRighttMotor"),
+                hwMap.dcMotor.get("frontRightMotor"),
                 hwMap.dcMotor.get("backLeftMotor"),
                 hwMap.dcMotor.get("backRightMotor"));
 
 
-        )
+
     }
 }
