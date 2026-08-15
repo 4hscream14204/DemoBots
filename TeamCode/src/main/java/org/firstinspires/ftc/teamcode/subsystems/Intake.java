c75bd1aba72ff9ba29248e2248c3cbb37fb2bb8a
TacoMaster635 <tuckerlithgow@4hscream.org>
2026-08-15 14:11:50 -0500

    Intake was done?

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Intake.java
@@ -0,0 +1,40 @@
+package org.firstinspires.ftc.teamcode.subsystems;
+
+import com.qualcomm.robotcore.hardware.Servo;
+
+public class Intake {
+    Servo servoL;
+    Servo servoR;
+
+    public Intake(Servo m_servoL, Servo m_servoR){
+        servoL = m_servoL;
+        servoR = m_servoR;
+    }
+
+    public enum intakePower{
+        INTAKEL(1),
+        INTAKER(0),
+        OUTTAKEL(0),
+        OUTTAKER(1),
+        STOP(.5);
+        intakePower(double m_power){
+            double power = m_power;
+        }
+    }
+
+
+    public void setOuttake(){
+        servoR.setPosition(1);
+        servoL.setPosition(0);
+    }
+
+    public void setIntake(){
+        servoR.setPosition(0);
+        servoL.setPosition(1);
+    }
+
+    public void setOff(){
+        servoR.setPosition(0.5);
+        servoL.setPosition(0.5);
+    }
+}
