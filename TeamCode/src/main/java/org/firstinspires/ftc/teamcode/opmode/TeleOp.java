265a8b9cc1426753036f12e4faeb9c8859a7f450
TacoMaster635 <tuckerlithgow@4hscream.org>
2026-08-15 14:50:00 -0500

    telemetry unhelpfullnesss

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/TeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/TeleOp.java
@@ -1,5 +1,19 @@
 package org.firstinspires.ftc.teamcode.opmode;
 
-public class TeleOp {
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+@com.qualcomm.robotcore.eventloop.opmode.TeleOp
+public class TeleOp extends OpMode {
 
+    @Override
+    public void init() {
+
+    }
+
+    @Override
+    public void loop() {
+
+
+
+        telemetry.addData("There is a '" + Math.random() + "' percent chance that this works", "");
+    }
 }
